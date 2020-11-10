1aa2c3a49dee34460941c2bb3b51dd0ea2f0b59b
guppyexpress <71186470+guppyexpress@users.noreply.github.com>
2020-11-10 13:21:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function GetRandomColor() {
 var Particle = function () {
     this.x = canvas.width * Math.random();
     this.y = canvas.height * Math.random();
-    this.vx = 50 * Math.random() - 50;
-    this.vy = 50 * Math.random() - 50;
+    this.vx = 50 * Math.random() - 25;
+    this.vy = 50 * Math.random() - 25;
     this.Color = GetRandomColor();
 }
 //Ading two methods
