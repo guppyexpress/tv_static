ee0275070555b7b2e4e764f4b3c729d59b18c437
guppyexpress <71186470+guppyexpress@users.noreply.github.com>
2020-11-11 08:55:34 -0600

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var particles = [];
-var num_particles = 40000;//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM MICROWAVE TIME
+var fuzzy_boi_count = 40000;//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM MICROWAVE TIME
 
 //Helps prevent colors from being too high or too low
 function GetRandomColor() {
@@ -52,6 +52,6 @@ function loop() {
     requestAnimationFrame(loop);
 }
 //Create particles
-for (var i = 0; i < num_particles; i++)
+for (var i = 0; i < fuzzy_boi_count; i++)
     particles.push(new Particle());
 loop();
