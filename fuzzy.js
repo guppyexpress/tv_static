912687d5b1f98197885513d865473c09716ab542
guppyexpress <71186470+guppyexpress@users.noreply.github.com>
2020-11-11 08:39:32 -0600

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -21,8 +21,8 @@ function GetRandomColor() {
 var Particle = function () {
     this.x = canvas.width * Math.random();
     this.y = canvas.height * Math.random();
-    this.vx = 100 * Math.random() - 100;
-    this.vy = 100 * Math.random() - 100;
+    this.vx = 500 * Math.random() - 500;
+    this.vy = 500 * Math.random() - 500;
     this.Color = GetRandomColor();
 }
 //Ading two methods
