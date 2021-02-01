4a3b11c21b917db3f4c7c16d36d3837269e3d873
guppyexpress <71186470+guppyexpress@users.noreply.github.com>
2021-02-01 09:49:42 -0600

    Update fuzzy.js

--- fuzzy.js
+++ fuzzy.js
@@ -4,9 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var particles = [];
 var num_particles = 40000;//MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM MICROWAVE TIME
-//multi Threading test
-var num_threads = 2;
-var MT = new Multithread(num_threads);
+
 //Helps prevent colors from being too high or too low
 function GetRandomColor() {
     var r = 0, g = 0, b = 0;
